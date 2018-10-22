7d2aafc1c523a19a171307ebe62169576910aded
Leocaliban <leocaliban@gmail.com>
2018-10-21 22:52:50 -0300

    Cria o projeto

---
+++ src/main/java/com/finance/api/FinanceApiSpringApplication.java
@@ -0,0 +1,12 @@
+package com.finance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApiSpringApplication.class, args);
+	}
+}
