28f8fe6861d3aeb1ccba8f2c66375118aea364a7
Leocaliban <leocaliban@gmail.com>
2018-10-25 04:21:33 -0300

    Ajusta a criação do HeaderLocation usando eventos de aplicação do spring

--- src/main/java/com/finance/api/resources/PessoaResource.java
+++ src/main/java/com/finance/api/resources/PessoaResource.java
@@ -1,12 +1,13 @@
 package com.finance.api.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,37 +15,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.finance.api.event.RecursoCriadoEvent;
 import com.finance.api.model.Pessoa;
 import com.finance.api.repositories.PessoaRepository;
 
 @RestController
-@RequestMapping("/pessoas") 
+@RequestMapping("/pessoas")
 public class PessoaResource {
-	
+
 	@Autowired
 	private PessoaRepository repository;
-	
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
-	public List<Pessoa> listar(){
+	public List<Pessoa> listar() {
 		return repository.findAll();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = repository.save(pessoa);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
-	
+
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPorCodigo(@PathVariable Long codigo) {
 		Pessoa pessoa = repository.findOne(codigo);
-		
+
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
 
