dbb9438323798a14f71a099e796eee4cda104a21
Leocaliban <leocaliban@gmail.com>
2018-10-25 01:56:18 -0300

    Implementa o POST em Categoria

--- src/main/java/com/finance/api/resources/CategoriaResource.java
+++ src/main/java/com/finance/api/resources/CategoriaResource.java
@@ -1,11 +1,18 @@
 package com.finance.api.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.finance.api.model.Categoria;
 import com.finance.api.repositories.CategoriaRepository;
@@ -23,4 +30,14 @@ public class CategoriaResource {
 
 	}
 
+	@PostMapping
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = repository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+
+	}
 }
