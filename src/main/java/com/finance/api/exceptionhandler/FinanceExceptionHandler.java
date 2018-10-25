dbae99b92e4c3adcaf8a579403e92144dbd9304a
Leocaliban <leocaliban@gmail.com>
2018-10-25 02:47:10 -0300

    Aplica validações com BeanValidation

--- src/main/java/com/finance/api/exceptionhandler/FinanceExceptionHandler.java
+++ src/main/java/com/finance/api/exceptionhandler/FinanceExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.finance.api.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,6 +35,29 @@ public class FinanceExceptionHandler extends ResponseEntityExceptionHandler {
 
 		ErroMessage erro = new ErroMessage(mensagemUsuario, mensagemDesenvolvedor);
 		return handleExceptionInternal(ex, erro, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	/**
+	 * Faz a captura da exceção causada por argumentos que não puderam ser validados
+	 * Salva em uma lista para apresentar as mensagens para todos os elementos
+	 * inválidos
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<ErroMessage> erros = criarListaDeErros(ex.getBindingResult());
+
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	private List<ErroMessage> criarListaDeErros(BindingResult bindingResult) {
+		List<ErroMessage> erros = new ArrayList<>();
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new ErroMessage(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		return erros;
 	}
 
 }
--- src/main/java/com/finance/api/resources/CategoriaResource.java
+++ src/main/java/com/finance/api/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +33,7 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = repository.save(categoria);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
