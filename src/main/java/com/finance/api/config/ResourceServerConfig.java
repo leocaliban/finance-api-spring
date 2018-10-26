d041d9d97deddcd5fa38295cc23c68abd15e57fe
Leocaliban <leocaliban@gmail.com>
2018-10-26 05:55:47 -0300

    Configura o Servidor de Recursos [OAuth2]

--- src/main/java/com/finance/api/config/ResourceServerConfig.java
+++ src/main/java/com/finance/api/config/ResourceServerConfig.java
@@ -1,29 +1,37 @@
 package com.finance.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity //habilita a segurança.
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer 
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/categorias").permitAll() //permite acesso sem autenticação para /categorias
 			.anyRequest()
 			.authenticated() //deve estar altenticado para acessar
-			.and().httpBasic() //tipo de autenticação
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and().csrf(); 
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 }
