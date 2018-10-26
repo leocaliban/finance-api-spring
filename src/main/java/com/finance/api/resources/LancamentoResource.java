2dabc283e752136cb1b1b061dae4f1884519e58d
Leocaliban <leocaliban@gmail.com>
2018-10-26 02:20:31 -0300

    Aplica a busca pelo id dos lançamentos

--- src/main/java/com/finance/api/resources/LancamentoResource.java
+++ src/main/java/com/finance/api/resources/LancamentoResource.java
@@ -3,7 +3,9 @@ package com.finance.api.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class LancamentoResource {
 	@GetMapping
 	public List<Lancamento> buscarTodos() {
 		return service.buscarTodos();
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPorCodigo(@PathVariable Long codigo){
+		Lancamento lancamento = service.buscarPorCodigo(codigo);
+		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
 	}
 }
--- src/main/java/com/finance/api/services/LancamentoService.java
+++ src/main/java/com/finance/api/services/LancamentoService.java
@@ -3,6 +3,7 @@ package com.finance.api.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.finance.api.model.Lancamento;
@@ -16,5 +17,14 @@ public class LancamentoService {
 
 	public List<Lancamento> buscarTodos() {
 		return repository.findAll();
+	}
+
+	public Lancamento buscarPorCodigo(Long codigo) {
+		Lancamento lancamento = repository.findOne(codigo);
+
+		if (lancamento == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return lancamento;
 	}
 }
