22b1acde54b637fbf1ae57fcd0d88911361f1e8c
Leocaliban <leocaliban@gmail.com>
2018-10-27 02:09:21 -0300

    Cria o filtro CORS

---
+++ src/main/java/com/finance/api/cors/CorsFilter.java
@@ -0,0 +1,52 @@
+package com.finance.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originPermitida = "http://localhost:4200";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest requisicao = (HttpServletRequest) request;
+		HttpServletResponse resposta = (HttpServletResponse) response;
+
+		resposta.setHeader("Access-Control-Allow-Origin", originPermitida);
+		resposta.setHeader("Access-Control-Allow-Credentials", "true");
+
+		if ("OPTIONS".equals(requisicao.getMethod()) && originPermitida.equals(requisicao.getHeader("Origin"))) {
+			resposta.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			resposta.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			resposta.setHeader("Access-Control-Max-Age", "3600");
+
+			resposta.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
